2f50c8d7d184745eff21c00febaa97a3ff7dcba2
Marcel Roozekrans <marcel@roozekrans.nl>
2026-03-08 17:38:58 +0100

    refactor: rename diagnostic IDs from NP to ZA prefix

--- src/ZeroAlloc.Analyzers/DiagnosticIds.cs
+++ src/ZeroAlloc.Analyzers/DiagnosticIds.cs
@@ -2,14 +2,14 @@ namespace ZeroAlloc.Analyzers;
 
 public static class DiagnosticIds
 {
-    public const string UseFrozenDictionary = "NP0001";
-    public const string UseFrozenSet = "NP0002";
-    public const string UseCollectionsMarshalAsSpan = "NP0003";
-    public const string UseSearchValues = "NP0004";
-    public const string AvoidStringConcatInLoop = "NP0005";
-    public const string UseStackalloc = "NP0006";
-    public const string UseArrayPool = "NP0007";
-    public const string AvoidEnumHasFlag = "NP0008";
-    public const string AvoidStringReplaceChain = "NP0009";
-    public const string UseTryGetValue = "NP0010";
+    public const string UseFrozenDictionary = "ZA0001";
+    public const string UseFrozenSet = "ZA0002";
+    public const string UseCollectionsMarshalAsSpan = "ZA0003";
+    public const string UseSearchValues = "ZA0004";
+    public const string AvoidStringConcatInLoop = "ZA0005";
+    public const string UseStackalloc = "ZA0006";
+    public const string UseArrayPool = "ZA0007";
+    public const string AvoidEnumHasFlag = "ZA0008";
+    public const string AvoidStringReplaceChain = "ZA0009";
+    public const string UseTryGetValue = "ZA0010";
 }
